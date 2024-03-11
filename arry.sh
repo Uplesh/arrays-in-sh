2a90126a5107fddf8a1f378aca458feb0571a82a
Uplesh <162953706+Uplesh@users.noreply.github.com>
2024-03-11 11:23:50 +0530

    arry.sh

---
+++ arry.sh
@@ -0,0 +1,17 @@
+  1 #!/bin/bash
+  2 <<doc
+  3 Name:
+  4 Date:
+  5 Description:
+  6 input:
+  7 output:
+  8 doc
+  9 echo "0th argument:$0"
+ 10 echo "1th argument:$1"
+ 11 echo "2th argument:$2"
+ 12
+ 13 echo "all arguments:$@"
+ 14 echo "all arguments:$*"
+ 15
+ 16 echo "count of argument:$#"
+ 17
